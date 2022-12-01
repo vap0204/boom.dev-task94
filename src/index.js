1625aacd9a350c18e09519b4a96194701fc111a3
Vesela Apostolova <100559743+vap0204@users.noreply.github.com>
2022-12-01 20:56:18 +0200

    dev

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import Application from "./js/Application";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const app = new Application();
+
   // Used to access the app instance by the automated tests
   app.setEmojis(["🐒", "🦍", "🦧"]);
-  app.addBananas();
+  const newMonkeys = [];
+  app.addBananas(newMonkeys);
+  console.log(newMonkeys);
+
+  const newDiv = document.getElementById("emojis");
+  const newParagraph = document.createElement("p");
+  newParagraph.textContent = newMonkeys;
+  newDiv.appendChild(newParagraph);
   window.__JS_APP = app;
 });
